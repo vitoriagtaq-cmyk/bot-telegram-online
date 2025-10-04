8a4267a41a735abae467c33c9d8b712767226796
vitoriagtaq-cmyk <vitoriagtaq@gmail.com>
2025-10-04 00:50:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 # ======================================================================
 
 # Credenciais do Telegram (agora usando variáveis de ambiente)
-API_ID = int(os.getenv('API_ID', '28183526'))
+API_ID = int(os.getenv('API_ID', '26183526'))
 API_HASH = os.getenv('API_HASH', '07e32e3de152f98a9b96365ecdde76cc')
 
 # Nome do arquivo de sessão
@@ -242,4 +242,5 @@ def get_logs():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
+
     app.run(host='0.0.0.0', port=port, debug=False)
